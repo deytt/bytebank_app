dce473ee2f7eceae72cc43eff6bdb227e8f37e79
david-cerqueira <david.cerqueira10@outlook.com>
2026-02-04 11:10:40 -0300

    adding formatters utils

---
+++ lib/core/utils/formatters.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+class Formatters {
+  static String formatCurrency(double value) {
+    final formatter = NumberFormat.currency(
+      locale: 'pt_BR',
+      symbol: 'R\$',
+      decimalDigits: 2,
+    );
+    return formatter.format(value);
+  }
+
+  static String formatDate(DateTime date) {
+    return DateFormat('dd/MM/yyyy').format(date);
+  }
+
+  static String formatDateTime(DateTime dateTime) {
+    return DateFormat('dd/MM/yyyy HH:mm').format(dateTime);
+  }
+}
