ecc5cf3a92be9fa72cc194e3cefe2954000ddece
david-cerqueira <david.cerqueira10@outlook.com>
2026-02-10 18:03:05 -0300

    creating splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -2,11 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
 import 'providers/auth_provider.dart';
+import 'screens/splash/splash_screen.dart';
 import 'screens/login/login_screen.dart';
 import 'screens/dashboard/dashboard_screen.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _showSplash = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _hideSplashScreen();
+  }
+
+  Future<void> _hideSplashScreen() async {
+    await Future.delayed(const Duration(milliseconds: 3000));
+    if (mounted) {
+      setState(() {
+        _showSplash = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +37,17 @@ class App extends StatelessWidget {
       title: 'Bytebank App',
       theme: AppTheme.theme,
       debugShowCheckedModeBanner: false,
-      home: Consumer<AuthProvider>(
-        builder: (context, authProvider, _) {
-          if (authProvider.isLoading) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-          
-          return authProvider.user != null 
-            ? const DashboardScreen() 
-            : const LoginScreen();
-        },
-      ),
+      home: _showSplash
+          ? const SplashScreen()
+          : Consumer<AuthProvider>(
+              builder: (context, authProvider, _) {
+                if (authProvider.isLoading) {
+                  return const Scaffold(body: Center(child: CircularProgressIndicator()));
+                }
+
+                return authProvider.user != null ? const DashboardScreen() : const LoginScreen();
+              },
+            ),
     );
   }
 }
