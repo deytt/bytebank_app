0ca5cb147355e97954082a885acab68304907d67
david-cerqueira <david.cerqueira10@outlook.com>
2026-05-03 22:10:51 -0300

    updating model import reference

--- lib/screens/transactions/transaction_list_screen.dart
+++ lib/screens/transactions/transaction_list_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../core/theme/app_theme.dart';
 import '../../features/auth/presentation/bloc/auth_bloc.dart';
 import '../../features/transactions/presentation/bloc/transaction_bloc.dart';
-import '../../models/transaction_model.dart';
+import '../../features/transactions/data/models/transaction_model.dart';
+import '../../features/transactions/domain/entities/transaction.dart' show TransactionType;
 import '../../widgets/transaction_card.dart';
 import 'transaction_form_screen.dart';
 
